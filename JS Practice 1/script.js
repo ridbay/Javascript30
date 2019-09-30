4255be005a6e9c229eeeacb733e074fbc85c6c27
Ridwan <balogunridwan@gmail.com>
2019-09-30 17:23:49 +0100

    JS practice 1

--- JS Practice 1/script.js
+++ JS Practice 1/script.js
@@ -1,6 +1,4 @@
-// Get your shorts on - this is an array workout!
-    // ## Array Cardio Day 1
-    // Some data we can work with
+
     const inventors = [
       { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
       { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -16,8 +14,22 @@
       { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
     ];
     const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+    
+    document.querySelector('.investors').innerHTML = inventors.map((investor, idx)=> {
+      const item = `<div> ${idx + 1}. first: ${investor.first}, last: ${investor.last}, year: ${investor.year}, passed: ${investor.passed} </div>`;
+      return item;
+    });
     // Array.prototype.filter()
-    // 1. Filter the list of inventors for those who were born in the 1500's
+    const que1 =  "1. Filter the list of inventors for those who were born in the 1500's";
+    document.querySelector('.que1').innerHTML = que1;
+    // document.getElementById("demo").innerHTML += 5 + 6;
+    const filterArray = inventors.filter( inventor => inventor.year > 1500 && inventor.year < 1600);
+    const mapFilterArray = filterArray.map(array => `<li> ${array.first} </li>`).join('')
+    const filterList = document.querySelector('#filter');
+    filterList.innerHTML = "<ul>" + mapFilterArray + "</ul>"
+
+
+
     // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
     // Array.prototype.sort()
@@ -32,4 +44,3 @@
     // 8. Reduce Exercise
     // Sum up the instances of each of these
     const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
-  </script>
