9592679318d143335f137244bf89dfe7ee4276ac
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-12 11:52:01 +0100

    Slide on Scroll

--- Slide in on Scroll/script.js
+++ Slide in on Scroll/script.js
@@ -12,3 +12,11 @@ function debounce(func, wait = 20, immediate = true) {
       if (callNow) func.apply(context, args);
     };
   }
+
+const slideImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e){
+    console.log(e)
+}
+
+window.addEventListener('scroll', checkSlide);
