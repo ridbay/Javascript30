66123c696688cd751a14aceba46d865e75a6fc95
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-16 10:52:15 +0100

    Array cardio 2

--- Array Cardio Day 2/script.js
+++ Array Cardio Day 2/script.js
@@ -7,13 +7,6 @@
     { name: 'Lux', year: 2015 }
   ];
 
-  const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
 
 
   // Some and Every Checks
@@ -31,6 +24,19 @@
     return ((currYear - person.year) >= 19)
   })
   console.log({isEveryoneAdult})
+
+
+
+
+
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
@@ -40,4 +46,16 @@
   console.log(isThere)
   // Array.prototype.findIndex()
   // Find the comment with this ID
+  const isIndex = comments.findIndex(comment => comment.id == 823423);
+  console.log(isIndex)
+
   // delete the comment with the ID of 823423
+// const deleteComment = [...comments.splice(isIndex, 1)]
+// console.table(filterCommet)
+
+const newComments = [
+    ...comments.slice(0, isIndex),
+    ...comments.slice(isIndex + 1)
+]
+
+console.table(newComments)
